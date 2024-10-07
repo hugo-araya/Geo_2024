f0edb39b6b6ccb52fd2a6ef6e1cbd063bcf7facb
Hugo Araya C. <42989121+hugo-araya@users.noreply.github.com>
2024-10-07 09:04:55 -0300

    jjj

--- descuento_3.py
+++ descuento_3.py
@@ -2,12 +2,12 @@
 Si se compran tres camisas o más se aplica un descuento del 20% sobre el total 
 de la compra y si son menos de tres camisas un descuento del 10%
 '''
-monto = int(input("monto "))
-compra = int(input("cantidad de prendas "))
+monto = 100
+compra = 5
  
 if compra >3:
    desc = monto * 0.2
 else:
    desc = monto * 0.1
 total = monto - desc
-print("monto a pagar " , desc )
+print(monto )
